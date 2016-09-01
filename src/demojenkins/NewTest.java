fa8137e51bd88a0e651941565b3d3b6f7347a68c
maheshvidhate <mahesh.vidhate@gslab.com>
2016-09-01 15:10:15 +0530

    sleep condition is removed

--- src/demojenkins/NewTest.java
+++ src/demojenkins/NewTest.java
@@ -9,13 +9,13 @@ public class NewTest {
   public void f() {
 	  System.out.println("test for facebook new123");
 	  WebDriver driver=new FirefoxDriver();
-	  try{Thread.sleep(3000);}catch(Exception e){}
+	  //try{Thread.sleep(3000);}catch(Exception e){}
 	  System.out.println("pass 1");
 	  driver.get("http://www.facebook.com");
 	  System.out.println("pass 2");
-	  try{Thread.sleep(3000);}catch(Exception e){}
+	  //try{Thread.sleep(3000);}catch(Exception e){}
 	  System.out.println(driver.getTitle());
-	  try{Thread.sleep(3000);}catch(Exception e){}
+	  //try{Thread.sleep(3000);}catch(Exception e){}
 	  System.out.println("pass 3");
 	  driver.quit();
   }
